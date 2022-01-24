f57d07fa2382a732ad673b90d72f9288704890e6
yubrot <yubrot@gmail.com>
2022-01-24 23:35:21 +0900

    Relax sync::mutex::Mutex constraints

--- ors-kernel/src/sync/mutex.rs
+++ ors-kernel/src/sync/mutex.rs
@@ -5,21 +5,11 @@ use core::ops::{Deref, DerefMut};
 
 /// `spin::Mutex` with `crate::interrupts::Cli` to avoid deadlocks.
 #[derive(Debug)]
-pub struct Mutex<T> {
+pub struct Mutex<T: ?Sized> {
     inner: spin::Mutex<T>,
 }
 
-impl<T> Mutex<T> {
-    pub const fn new(value: T) -> Self {
-        Self {
-            inner: spin::Mutex::new(value),
-        }
-    }
-
-    pub fn into_inner(self) -> T {
-        self.inner.into_inner()
-    }
-
+impl<T: ?Sized> Mutex<T> {
     pub fn lock(&self) -> MutexGuard<T> {
         let cli = Cli::new();
         let inner = self.inner.lock();
@@ -37,12 +27,24 @@ impl<T> Mutex<T> {
     }
 }
 
-pub struct MutexGuard<'a, T> {
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            inner: spin::Mutex::new(value),
+        }
+    }
+
+    pub fn into_inner(self) -> T {
+        self.inner.into_inner()
+    }
+}
+
+pub struct MutexGuard<'a, T: 'a + ?Sized> {
     inner: spin::MutexGuard<'a, T>,
     cli: Cli,
 }
 
-impl<'a, T> MutexGuard<'a, T> {
+impl<'a, T: 'a + ?Sized> MutexGuard<'a, T> {
     pub fn leak(this: Self) -> &'a mut T {
         let inner = spin::MutexGuard::leak(this.inner);
         mem::forget(this.cli);
@@ -50,7 +52,7 @@ impl<'a, T> MutexGuard<'a, T> {
     }
 }
 
-impl<'a, T> Deref for MutexGuard<'a, T> {
+impl<'a, T: 'a + ?Sized> Deref for MutexGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -58,19 +60,19 @@ impl<'a, T> Deref for MutexGuard<'a, T> {
     }
 }
 
-impl<'a, T> DerefMut for MutexGuard<'a, T> {
+impl<'a, T: 'a + ?Sized> DerefMut for MutexGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut *self.inner
     }
 }
 
-impl<'a, T: fmt::Debug> fmt::Debug for MutexGuard<'a, T> {
+impl<'a, T: 'a + fmt::Debug + ?Sized> fmt::Debug for MutexGuard<'a, T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Debug::fmt(&**self, f)
     }
 }
 
-impl<'a, T: fmt::Display> fmt::Display for MutexGuard<'a, T> {
+impl<'a, T: 'a + fmt::Display + ?Sized> fmt::Display for MutexGuard<'a, T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Display::fmt(&**self, f)
     }
